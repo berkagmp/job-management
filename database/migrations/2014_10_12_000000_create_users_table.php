f3057af7ca23d6586fb9c22c64b418f406fb0b46
Derek <berkagmp@gmail.com>
2020-07-01 13:55:09 +1200

    Set database migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,36 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id');
+            $table->string('uid')->unique();
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('name', 100);
+            $table->string('username', 100);
+            $table->string('email');
+            $table->string('phone', 20);
+            $table->string('password', 200);
+            $table->string('remember_token', 255)->nullable();
             $table->timestamps();
+        });
+
+        Schema::create('types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+        });
+
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->index();
+            $table->unsignedInteger('type_id')->index();
+            $table->date('date');
+            $table->time('time', 0);
+            $table->string('location', 255);
+            $table->float('price', 15, 2);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
@@ -31,6 +54,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('types');
         Schema::dropIfExists('users');
     }
 }
